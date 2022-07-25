e70a58a7373f4e6a0a0db5e662c34a0a99b488b4
RKasaputra <dikasaputra190903@gmail.com>
2022-07-25 13:02:31 +0700

    update dika

---
+++ r_dika/Kemahasiswaan_reihan_nasywan_mustofa_kamil_10121113.java
@@ -0,0 +1,13 @@
+package kemahasiswaan_reihan_nasywan_mustofa_kamil_10121113;
+
+public class Kemahasiswaan_reihan_nasywan_mustofa_kamil_10121113 {
+
+    public static void main(String[] args) {
+        f_mahasiswa_nilai m_nilai = new f_mahasiswa_nilai();
+        m_nilai.nonaktif_teks();
+        m_nilai.Combo_mahasiswa();
+        m_nilai.Combo_matakuliah();
+        m_nilai.setVisible(true);
+    }
+    
+}
